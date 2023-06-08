9918f5cc30796dde0320b00c4384e2e972abd9f4
AlexGrohmann <alex.grohmann@googlemail.com>
2023-06-08 20:50:38 +0200

    add demo and create functional react components

--- convertSvg.js
+++ convertSvg.js
@@ -3,7 +3,7 @@ console.log("script started");
 const fs = require("fs");
 
 const startPath = "./svg/";
-const targetPath = "./react-components/";
+const targetPath = "./demo/src/";
 
 fs.readdir(startPath, (err, folder) => {
   console.log(folder);
@@ -17,13 +17,23 @@ fs.readdir(startPath, (err, folder) => {
         return;
       }
 
-      fs.writeFile(targetPath + file.slice(0, -4) + ".tsx", data, (err) => {
-        if (err) {
-          console.error(err);
+      fs.writeFile(
+        targetPath + file.slice(0, -4) + ".tsx",
+        "import React from 'react';" +
+          "\nfunction App() {" +
+          "\n\treturn (\n\t" +
+          data +
+          "\n\t);" +
+          "\n}" +
+          "\nexport default App;",
+        (err) => {
+          if (err) {
+            console.error(err);
+          }
+          console.log("created: ", file.slice(0, -4) + ".tsx");
+          // file written successfully
         }
-        console.log("created: ", file.slice(0, -4) + ".tsx");
-        // file written successfully
-      });
+      );
     });
   });
 });
